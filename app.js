d0097aa1f130951238eb41916976d6154186d47b
itnhtu <itnhtu@gmail.com>
2023-03-17 15:23:36 +0700

    completed jwt basic

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-const express = require('express')
-require('express-async-errors')
-const app = express()
 require('dotenv').config()
-const connectDB = require('./db/connect')
+require('express-async-errors')
+const express = require('express')
+const app = express()
+
+const mainRouter = require('./routes/main')
 const notFoundHandler = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
 //middlewares
 app.use(express.json())
+app.use(express.static('./public'))
 
 //routes
-app.get('/', (req, res) => {
-    res.send('<h1>Store API </h1> <a href="/api/v1/products">Products route</a>')
-})
-
-app.use('/api/v1/products', productsRouter)
+app.use('/api/v1', mainRouter)
 
 //product routes
 app.use(notFoundHandler)
@@ -23,8 +21,6 @@ app.use(errorMiddleware)
 const port = process.env.PORT || 3000
 const start = async () => {
     try {
-        //connect to database
-        await connectDB(process.env.MONGO_URI)
         app.listen(port,console.log(`Listening on port ${port}`))
     } catch (error) {
         console.log(error)
